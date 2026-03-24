55df21cfa4a04f17e9a8832979b45ee25c4e321c
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2026-03-24 10:59:32 +0600

    update

--- AI-agent/ai-agent.js
+++ AI-agent/ai-agent.js
@@ -1,4 +1,5 @@
 const OpenAI = require('openai');
+const { InputTokens } = require('openai/resources/responses/input-tokens.js');
 require('dotenv').config()
 
 const openai = new OpenAI({
@@ -10,10 +11,55 @@ async function main() {
   const completion = await openai.chat.completions.create({
     model: "stepfun/step-3.5-flash:free",
     messages: [
-      { role: "user", content: "Say this is a test" }
+      { role: "user", content: "What is react?" }
     ],
   })
 
   console.log(completion.choices[0].message)
 }
-main();
+
+
+
+const anaLizeGoal = async () => {
+  const goalText = "Learn Python";
+  const durationDays = "7";
+  const prompt = `Users want to ${goalText} within ${durationDays} days. Create a plan to ${goalText} in ${durationDays} days.
+  
+   Create plan stractured in 5 steps:
+   Create a list of tasks.
+
+
+   Return as json with this structure
+   
+   example.
+   
+   {
+    "plan": [
+      "Task 1",
+      "Task 2",
+      "Task 3",
+      "Task 4",
+      "Task 5"
+    ]
+  }
+  `;
+
+
+  try {
+
+    const completion = await openai.chat.completions.create({
+      model: "stepfun/step-3.5-flash:free",
+      messages: [
+        { role: "user", content: prompt }
+      ],
+    })
+    console.log(completion.choices[0].message)
+  } catch (error) {
+    console.error(error)
+
+  }
+}
+
+
+// main()
+anaLizeGoal()
---
+++ AI-agent/output.md
@@ -0,0 +1,11 @@
+'{\n' +
+    '  "plan": [\n' +
+    '    "Master Python basics: syntax, variables, data types, and basic operations (Days 1-2
+)",\n' +
+    '    "Implement control flow with conditionals and loops (Day 3)",\n' +
+    '    "Define and use functions, explore modules and libraries (Day 4)",\n' +
+    '    "Manipulate data using lists, dictionaries, sets, and tuples (Day 5)",\n' +
+    '    "Apply skills in a mini-project, covering file I/O, error handling, and code review 
+(Days 6-7)"\n' +
+    '  ]\n' +
+    '}'
